842a35cd22b1f8d81e855b7a4035d7c4ce3032fc
Aymeric <aymeric.lamboley@gmail.com>
2017-01-25 14:15:31 +0100

    Unity code with completed task information

--- Unity/Assets/Script.cs
+++ Unity/Assets/Script.cs
@@ -1,10 +1,26 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Script : MonoBehaviour {
 
 	void Start () {
 
-		System.Diagnostics.Process.Start(Application.dataPath + "/../../pngquant-osx", Application.dataPath + "/../../png-to-compress");
-	}
+		System.Diagnostics.Process process = new  System.Diagnostics.Process();
 
+		process.EnableRaisingEvents = true;
+		process.Exited += (sender, e) => { Debug.Log("process completed"); };
+
+		string exec = "";
+		if (Application.platform == RuntimePlatform.WindowsEditor || Application.platform == RuntimePlatform.WindowsPlayer)
+			exec = "pngquant-windows.cmd";
+		else if (Application.platform == RuntimePlatform.OSXEditor || Application.platform == RuntimePlatform.OSXPlayer)
+			exec = "pngquant-osx";
+		else
+			throw new Exception("Platform not supported");
+
+		process.StartInfo.FileName = Application.dataPath + "/../../" + exec;
+		process.StartInfo.Arguments = Application.dataPath + "/../../png-to-compress";
+
+		process.Start();
+	}
 }

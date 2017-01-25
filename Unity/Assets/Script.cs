859e6971472694284f6f6cb6f92303ff640596f7
Aymeric <aymeric.lamboley@gmail.com>
2017-01-25 13:53:59 +0100

    Compression working with Unity project

---
+++ Unity/Assets/Script.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Script : MonoBehaviour {
+
+	void Start () {
+
+		System.Diagnostics.Process.Start(Application.dataPath + "/../../pngquant-osx", Application.dataPath + "/../../png-to-compress");
+	}
+
+}
